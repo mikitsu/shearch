918581c18faa48b395bbc0f30626b6ab9001a288
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2021-05-24 15:45:23 +0200

    allow setting UDS permissions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "log"
   "flag"
+  "os"
   "net"
   "net/http"
   "strings"
@@ -11,13 +12,12 @@ import (
 )
 
 func main() {
-  laddr, conf := getConfig()
-  listener := getListener(laddr)
+  listener, conf := getConfig()
   log.Println("listening on", listener.Addr())
   http.Serve(listener, &conf)
 }
 
-func getListener(laddr string) net.Listener {
+func getListener(laddr string, sock_mode uint) net.Listener {
   var lnet string
   if strings.HasPrefix(laddr, "unix:") {
     laddr = laddr[5:]
@@ -29,13 +29,21 @@ func getListener(laddr string) net.Listener {
   if err != nil {
     log.Fatal(err)
   }
+  if lnet == "unix" {
+    err := os.Chmod(laddr, os.FileMode(sock_mode))
+    if err != nil {
+      log.Println("error setting socket mode: ", err)
+    }
+  }
   return listener
 }
 
-func getConfig() (string, lib.Config) {
+func getConfig() (net.Listener, lib.Config) {
   var laddr, prefix, redirect, separator, opensearch string
+  var mode uint
   flag.StringVar(&laddr, "listen", "127.0.0.1:8080",
                  "listen on; may also be a unix:/domain.socket")
+  flag.UintVar(&mode, "sock-mode", 0777, "file permissions when listening on UDS")
   flag.StringVar(&prefix, "prefix", "!", "default shortcut prefix")
   flag.StringVar(&redirect, "redirect", "https://duckduckgo.com/?q=",
                  "default redirect location if no shortcut matches")
@@ -43,5 +51,5 @@ func getConfig() (string, lib.Config) {
                  "default separator between shortcut and query")
   flag.StringVar(&opensearch, "url", "", "URL shearch is available on (for opensearch)")
   flag.Parse()
-  return laddr, lib.GetConfig(prefix, redirect, separator, opensearch)
+  return getListener(laddr, mode), lib.GetConfig(prefix, redirect, separator, opensearch)
 }
